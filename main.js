cfbacf1e0f62115b354cffdf6c411554cb36a49d
Parth Kaloliya <kaloliya@gmail.com>
2019-01-16 18:39:59 +0530

    Funcitons : Sqrt, square, cube, factorial, all-clear, clear done

--- main.js
+++ main.js
@@ -1,79 +1,56 @@
- //Function of Display Number
- function displayNumber(number){
-    calcForm.calcDisplay.value += number;
+//Function of Displaying Numbers
+function displayDigit(number){
+    console.log(number);
+    var result = document.getElementById("resultBlock");
+    result.textContent += number;
 }
 
-//Function of Display Operator
-function displayOperator(opr){
-    var value1,value2,result;
-    value1 = calcForm.calcDisplay.value;
-    console.log(value1);
-    if(opr === '+'){
-        console.log('+');
-        calcForm.calcDisplay.value = "";
-        value2 = calcForm.calcDisplay.value;
-        console.log(value2);
-        if(opr === '='){
-            console.log('=');
-            console.log(value1);
-            console.log(value2);
-            result = value1 + value2;
-            calcForm.calcDisplay.value = result;
-        }
-    }else if(opr === '-'){
-        console.log('-');
-    }else if(opr === '*'){
-        console.log('*');
-    }else if(opr === '/'){
-        console.log('/');
-    }else{
-
-    }
-}
-
-//Function of All Clear
-function allClear(){
-    calcForm.calcDisplay.value = "";
-}
-
-//Function of Clear Single Digit
-function deleteSingleDigit(){
-    var result = calcForm.calcDisplay.value;
-    calcForm.calcDisplay.value = result.substr(0,result.length - 1);
-}
-
-//Function of Clear Entire Number
-function clearNumber(){
-    calcForm.calcDisplay.value = "";
-}
-
-//Function of Cube
+//Function of Count Cube
 function countCube(){
-    var result = Math.pow(calcForm.calcDisplay.value,3);
-    calcForm.calcDisplay.value = result;
+    var number = document.getElementById("resultBlock").innerHTML;
+    var result = Math.pow(number,3);
+    var display = document.getElementById("resultBlock");
+    display.textContent = result;
 }
 
-//Function of Square
+//Function of Count Square
 function countSquare(){
-    var result = Math.pow(calcForm.calcDisplay.value,2);
-    calcForm.calcDisplay.value = result;
+    var number = document.getElementById("resultBlock").innerHTML;
+    var result = Math.pow(number,2);
+    var display = document.getElementById("resultBlock");
+    display.textContent = result;
 }
 
-//Function of Square root
-function countSqrt(){
-    var result = Math.sqrt(calcForm.calcDisplay.value);
-    calcForm.calcDisplay.value = result;
-}
-
-//Function of Factorial
+//Function of Count Factorial
 function countFactorial(){
-    var result = calcForm.calcDisplay.value;
+    var number = document.getElementById("resultBlock").innerHTML;
     var fact,i;
     fact = 1;
     i = 1;
-    while(i<=result){
+    while(i <= number){
         fact = fact * i;
         i++;
     }
-    calcForm.calcDisplay.value = fact;
+    document.getElementById("resultBlock").textContent = fact;
 }
+
+//Function of Count Square root
+function countSqrt(){
+    var result = Math.sqrt(document.getElementById("resultBlock").textContent);
+    document.getElementById("resultBlock").textContent = result;
+}
+
+//Function of All clear
+function allClear(){
+    var result = document.getElementById("resultBlock").textContent;
+    result = "";
+    document.getElementById("resultBlock").textContent = result;
+}
+
+//Function of Delete single digit
+function deleteSingleDigit(){
+    var beforeValue = document.getElementById("resultBlock").textContent;
+    var result = beforeValue.substr(0,beforeValue.length - 1);
+    document.getElementById("resultBlock").textContent = result;
+}
+
